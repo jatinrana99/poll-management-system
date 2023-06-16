7e8607a7bf3bdf10184dee0d2bf01c9e20e7c897
Jatin rana <engr.jatinrana@gmail.com>
2023-06-16 21:20:54 +0530

    some code for edit is written

--- src/components/PollCards.vue
+++ src/components/PollCards.vue
@@ -1,17 +1,24 @@
 <template>
     <div>Hello from Poll cards</div>
-  <v-sheet width="300" class="mx-auto" >
-    <v-form fast-fail v-for="data in getPoll" :key="data">
+  <v-sheet width="500" class="mx-auto" >
+    <v-form fast-fail v-for="data in getPoll" :key="data" class="form">
+        <v-row>
+    <!-- <p>{{ data.title }}</p> -->
       <v-text-field
-        v-model="firstName"
         :rules="firstNameRules"
+        v-model="newTitle"
       >{{ data.title }}</v-text-field>
 
+    <v-btn >
+      <i class="fa-solid fa-pen-to-square" v-on:click="updateTitle(newTitle,data._id)"></i>
+    </v-btn>
+      </v-row>
 <v-radio-group>
   <!-- <v-radio value="1">{{ data.option }}</v-radio> -->
 </v-radio-group>
 
       <v-btn block class="mt-2" @click="deletePoll(data._id)">Delete</v-btn>
+    
     </v-form>
   </v-sheet>
 
@@ -24,19 +31,30 @@
 
 <script>
 import {mapGetters} from 'vuex';
-import {mapActions} from 'vuex'
+import {mapActions} from 'vuex';
+
 
 export default {
     name:`PollCards`,
     data(){
         return{
-
+            newTitle:"",
+            // firstNameRules:[
+            //      () =>{
+            //         return this.title
+            //     }
+            // ]
         }
     },
     computed: mapGetters(['getPoll']),
 
     methods:{
-        ...mapActions(['getAllPoll', 'deletePoll'])
+        ...mapActions(['getAllPoll', 'deletePoll','updateTitle']),
+        editTitle(){
+            return{
+                
+            }
+        }
     },
 }
 </script>
--- src/store/modules/poll.js
+++ src/store/modules/poll.js
@@ -8,10 +8,10 @@ const actions={
     async addPoll({commit},pollDetails){
         const res = await axios.post(`http://65.108.77.50:3031/add_poll?title=${pollDetails.title}%20&options=${pollDetails.options[0]}____${pollDetails.options[1]}____${pollDetails.options[2]}____${pollDetails.options[3]}`);
         commit("add_poll",res.data);
-        console.log(res.data);
-        console.warn("Hello from poll title",pollDetails.options[0]);
-        console.warn("Hello from poll title2",pollDetails.options[1]);
-        console.warn("Hello from poll title3",pollDetails.options[2]);
+        // console.log(res.data);
+        // console.warn("Hello from poll title",pollDetails.options[0]);
+        // console.warn("Hello from poll title2",pollDetails.options[1]);
+        // console.warn("Hello from poll title3",pollDetails.options[2]);
         
 
     },
@@ -20,13 +20,18 @@ const actions={
     async getAllPoll({commit}){
         const res = await axios.get(`http://65.108.77.50:3031/list_polls`);
         commit("setPoll" , res.data.data);
-        console.log("Hello from api Poll" , res.data.data);
-        console.warn("Hello from api Poll Title" , res.data.data.id);
+        // console.log("Hello from api Poll" , res.data.data);
+        // console.warn("Hello from api Poll Title" , res.data.data._id);
     },
 
     async deletePoll({commit},id){
         let res=await axios.delete(`http://65.108.77.50:3031/delete_poll?id=${id}`);
-        commit("removePoll" ,res.data)
+        commit("removePoll" ,res.data);
+    },
+
+    async updateTitle({commit},newTitle,id){
+        let res= await axios.post(`http://65.108.77.50:3031//update_poll_title?id=${id}&title=${newTitle}`);
+        commit(("setTitle"),res.data);
     }
 
 };
@@ -34,7 +39,8 @@ const mutations={
     add_poll:(state,poll) => state.poll.push(poll),
     setPoll:(state,poll) => (state.poll=poll),
     removePoll:(state,id) => (state.poll.filter(poll=>
-                                                poll.id !== id))
+                                                poll.id !== id)),
+    // setTitle:(state,t) => (state.title=title)
 
 };
 const getters={
